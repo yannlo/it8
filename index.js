7639ad2fd0f268f8ec4688b77effcecf92e2fec4
yannlo <73764168+yannlo@users.noreply.github.com>
2021-01-05 22:26:04 +0000

    v1 final version

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// gestion du menu
+
 let check = document.getElementById("check");
 check.addEventListener("click",function(event){
     event.stopPropagation();
@@ -6,22 +8,35 @@ check.addEventListener("click",function(event){
     let menu = document.getElementById("menu");
     let menu_element = document.getElementById("menu_element");
     let icon = document.getElementById("icon");
+    let content = document.getElementById("content");
 
     if(menu_element.style.display == "block"){
         icon.style.transform= "rotate(0deg)";
-        icon.style.transition=".5s";
+        icon.style.transition=".3s";
         menu_element.style.display="none";
         menu.style.width="60px";
-        menu.style.transition=".5s";
-        menu_element.style.transition=".2s";
+        menu.style.transition=".3s";
+        menu_element.style.transition=".6s";
+        content.style.margin=" 0 0  0 25%";
+        content.style.transition=".3s";
+        content.style.width="75%";
+
+
+
 
     }else{
 
         menu_element.style.display="block";
         menu.style.width="30%";
-        menu.style.transition=".5s";
-        menu_element.style.transition=".2s";
+        menu.style.transition=".3s";
+        menu_element.style.transition=".6s";
         icon.style.transform= "rotate(90deg)";
-        icon.style.transition=".5s";
+        icon.style.transition=".3s";
+        content.style.margin=" 0 0  0 35%";
+        content.style.width="60%";
+
+        content.style.transition=".3s";
+
     }
 });
+
